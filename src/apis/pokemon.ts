6d83fbdade6f7dd3d56db0d3101f0e8abe9f1eab
fjw1010 <fjw_1010@naver.com>
2024-07-05 00:42:06 +0900

    과제 완료

---
+++ src/apis/pokemon.ts
@@ -0,0 +1,7 @@
+const ferchPokemonData = async (id: string) => {
+  const apiUrl = "http://localhost:3000";
+  const response = await fetch(`${apiUrl}/api/pokemons/${id}`);
+  return response.json();
+};
+
+export default ferchPokemonData;
---
+++ src/app/pokemon/[id]/page.tsx
@@ -0,0 +1,39 @@
+import ferchPokemonData from "@/apis/pokemon";
+import React from "react";
+import Link from "next/link";
+import Image from "next/image";
+
+const PokemonDetailPage = async ({ params }: { params: { id: string } }) => {
+  const pokemonData = await ferchPokemonData(params.id);
+
+  return (
+    <>
+      <div className="pokemon-details max-w-xl mx-auto bg-white shadow-lg rounded-lg overflow-hidden">
+        <div className="bg-gray-100 text-gray-800 text-center p-4">
+          <h2 className="text-2xl font-bold">{pokemonData.korean_name}</h2>
+          <p>No .{pokemonData.id.toString().padStart(4, "0")}</p>
+        </div>
+        <div className="p-4 text-black flex flex-col justify-start items-center">
+          <Image src={pokemonData.sprites.front_default} alt="포켓몬 이미지" />
+          <p className="text-center text-xl my-2">
+            이름 : {pokemonData.korean_name}
+          </p>
+          <div className="flex gap-2">
+            <p className="text-center">키 : {pokemonData.height / 10} m</p>
+            <p className="text-center">무게 : {pokemonData.weight / 10} kg</p>
+          </div>
+        </div>
+        <div className="text-center my-2">
+          <p className="font-bold mb-5">기술 : </p>
+          <div className="flex flex-wrap gap-2 items-center text-center justify-center">
+            {pokemonData.moves.map((move: any) => (
+              <div key={move.move.name}>{move.move.korean_name}</div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default PokemonDetailPage;
